db27622eb444b705819ca447813fb00b9f7170ee
Eduardo Ferreira <83325995+EduardoFMC@users.noreply.github.com>
2025-02-01 15:24:12 -0300

    Update 008.sh

--- submission/008.sh
+++ submission/008.sh
@@ -2,10 +2,11 @@
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
 
 txid="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
-raw_tx=$(bitcoin-cli getrawtransaction "$txid")
-decoded_tx=$(bitcoin-cli decoderawtransaction "$raw_tx")
-scriptSig=$(echo "$decoded_tx" | jq -r '.vin[0].scriptSig.hex')
-public_key=$(echo "$scriptSig" | grep -oE '[a-fA-F0-9]{66}$')
+tx=$(bitcoin-cli getrawtransaction "$txid" true | jq -r .)
+txinwitness=$(echo "$tx" | jq -r '.vin[0].txinwitness[]' | tail -n 1)
+pubkey_len_hex=${txinwitness:2:2}
+pubkey_len=$(printf "%d" "0x$pubkey_len_hex")
+pubkey=${txinwitness:4:pubkey_len*2}
 
 # out
-echo "$public_key"
+echo "$pubkey"
