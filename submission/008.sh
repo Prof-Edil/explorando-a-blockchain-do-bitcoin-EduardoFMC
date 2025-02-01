d415878b76fbd98f6c5964ce097146852481ff0b
Eduardo Ferreira <83325995+EduardoFMC@users.noreply.github.com>
2025-02-01 15:14:49 -0300

    Update 008.sh

--- submission/008.sh
+++ submission/008.sh
@@ -5,7 +5,7 @@ txid="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
 raw_tx=$(bitcoin-cli getrawtransaction "$txid")
 decoded_tx=$(bitcoin-cli decoderawtransaction "$raw_tx")
 scriptSig=$(echo "$decoded_tx" | jq -r '.vin[0].scriptSig.hex')
-public_key=$(echo "$scriptSig" | grep -oE '[a-fA-F0-9]{130,}' | tail -n 1)
+public_key=$(echo "$scriptSig" | grep -oE '[a-fA-F0-9]{66}$')
 
 # out
 echo "$public_key"
