e1c60661a5986574276254d337a115a611e87cd2
Eduardo Ferreira <83325995+EduardoFMC@users.noreply.github.com>
2025-02-01 15:32:41 -0300

    Update 008.sh

--- submission/008.sh
+++ submission/008.sh
@@ -2,8 +2,11 @@
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
 
 txid=e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163
-txinwitness=$(bitcoin-cli getrawtransaction "$txid" true | jq -r '.vin[0].txinwitness[-1]')
-pubkey_len=$(printf "%d" "0x${txinwitness:2:2}")
+tx=$(bitcoin-cli getrawtransaction "$txid" true | jq -r .)
+txinwitness=$(echo $tx | jq -r '.vin[0].txinwitness[]' | tail -n 1)
+
+pubkey_len_hex=${txinwitness:2:2}
+pubkey_len=$(printf "%d" "0x$pubkey_len_hex")
 pubkey=${txinwitness:4:pubkey_len*2}
 
-echo "$pubkey"
+echo $pubkey
